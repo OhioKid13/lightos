653592b8d0658db80691fa92a2ee5b093ae62246
OhioKid13 <154306092+OhioKid13@users.noreply.github.com>
2024-05-26 15:48:25 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Form submitted!');
+    alert('Form submitted! We will get back to you soon.');
 });
